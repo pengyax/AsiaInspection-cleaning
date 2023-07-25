35d032acb93a2b11701c3d62d5ee3747966bb184
Yaxiong <95468419@qq.com>
2023-07-25 17:38:40 +0800

    修改OverDue算法，追踪5批次订单。增加newItem追踪。

--- out_prioritylist.py
+++ out_prioritylist.py
@@ -5,9 +5,10 @@ from sql_engine import connect
 from datetime import datetime
 
 
-def out_status(df_add,df_new,df_product_family):
-    df = pd.concat([df_add,df_new],ignore_index=True)
-    
+def out_status(df_add,df_product_family):
+    df = df_add.copy()
+    df['Inspection Date'] = pd.to_datetime(df['Inspection Date'])
+    print(df.info())
     df_product_family = (
     df_product_family
     .assign(**{
@@ -16,8 +17,9 @@ def out_status(df_add,df_new,df_product_family):
     })
     .drop_duplicates(subset=['Material Number'],keep='last')
 )
+    df_uninspection = df.loc[df['Results'].isin(['G','W'])]
     
-    df = (
+    df_inspection = (
     df
     .query('Results in ("A","R")')
     .assign(**{
@@ -32,21 +34,21 @@ def out_status(df_add,df_new,df_product_family):
 )
     
     df_QIM_duplicate = (
-    df
+    df_inspection
     .query('Path.str.startswith("QIM",na=False)')
     .sort_values(['ID','Inspection Date'],ascending=[False,False])
     .drop_duplicates(subset=['ID'],keep='last')
 )
     
     df_offline_duplicate = (
-    df
+    df_inspection
     .query('~Path.str.startswith("QIM",na=False)')
     .sort_values(['ID','Inspection Date'],ascending=[False,False])
     .drop_duplicates(subset=['Lot Number','Vendor','Item Number','Inspector','Inspection Date'],keep='last')
 )
 
     df_duplicate = pd.concat([df_QIM_duplicate,df_offline_duplicate])
-    df_duplicate['Inspection Date'] = pd.to_datetime(df_duplicate['Inspection Date'])
+    # df_duplicate['Inspection Date'] = pd.to_datetime(df_duplicate['Inspection Date'])
     print('去重完成')
     print('===='*6)
 
@@ -84,23 +86,50 @@ def out_status(df_add,df_new,df_product_family):
     .loc[:,'item_key']
     .drop_duplicates() 
 )   
-    
-    df_overdue = (
-    df_duplicate
-    .sort_values(['Vendor','Inspection Date',],ascending=[False,False])
-    .groupby('Vendor')[['ID','Vendor','Inspection Date']].head(1)
-    .assign(Days_Difference =  lambda d : (datetime.today() - d["Inspection Date"]).dt.days)
-    .assign(Overdue = lambda d : d['Days_Difference'].apply(lambda s : True if s >90 else False))
-    .query('Overdue == True')
-    )
     print('rej_key完成')
     print('===='*6)
+    
+    
+    df_base = pd.concat([df_duplicate,df_uninspection],ignore_index=True)
+    
+    df_timeDiff = (
+    df_base
+    .sort_values(['Vendor','Inspection Date'],ascending=[False,False])
+    .reset_index(drop=True)
+    .assign(Time_Diff = lambda d : d.groupby('Vendor')['Inspection Date'].diff().dt.days.abs())
+    .assign(Time_Diff = lambda d : d['Time_Diff'].fillna((datetime.today() - d["Inspection Date"]).dt.days))
+)
+    idx = df_timeDiff.groupby("Vendor").apply(lambda x: x[x["Time_Diff"] > 90].index[0] if any(x["Time_Diff"] > 90) else None)
+    
+    df_overdue = (
+    df_timeDiff
+    .loc[idx.dropna()]
+    .reset_index()
+    .pipe(lambda d : pd.merge(d,df_timeDiff.groupby("Vendor").head(1).reset_index(),how='left',on=['Vendor']))
+    .assign(Count = lambda d : d['index_x'] - d['index_y'] + 1)
+    .query('Count < 5')
+    .loc[:,['Vendor','Time_Diff_x','Inspection Date_x','Inspection Date_y','Count']]
+)
+    print('overdue完成')
+    print('===='*6)
+    
+    df_unNewItem = (
+    df_duplicate
+    .query('Results in ("A","R")')
+    .groupby(['Vendor','Item Number'],as_index=False).size()
+    .query('size >= 5')
+    .assign(**{
+        "newItem" : lambda d : d['Item Number'].str.cat(d['Vendor'])
+        })
+)
+    
     
     with pd.ExcelWriter('../output/Audit Status.xlsx') as writer:
         df_item.to_excel(writer,sheet_name='items')
         df_product.to_excel(writer,sheet_name='product_family')
         df_reject.to_excel(writer,sheet_name='rek_key')
         df_overdue.to_excel(writer,sheet_name='overdue')
+        df_unNewItem.to_excel(writer,sheet_name='unNewItem')
     print('写入完成')
     print('===='*6)
 
@@ -113,14 +142,31 @@ if __name__ == "__main__":
     # df_sharepoint = pd.read_excel('../Book1.xlsx')
     fn_engine = connect('fn_mysql')
     
-    df = pd.read_sql('select * from inspection_data_all',fn_engine)
+    sql_query = f'''
+                select
+                    ID,
+                    `PO Number`,
+                    `Lot Number`,
+                    `Vendor Code`,
+                    Vendor,
+                    Division,
+                    `Inspection Date`,
+                    Inspector,
+                    `Item Number`,
+                    Results,
+                    `Reject Code`,
+                    Path
+                from
+                    inspection_data_all
+                '''
+    df = pd.read_sql(sql_query,fn_engine)
     print('加载历史数据完成')
     print('===='*6)
     
     df_product_family = pd.read_excel('../output/product_family.xlsx',sheet_name=0)
-    df_new = pd.read_excel('../output/addNew.xlsx')
+    # df_new = pd.read_excel('../output/addNew.xlsx')
     
     print('运算开始')
     print('===='*6)
-    out_status(df,df_new,df_product_family)
+    out_status(df,df_product_family)
     print('完成')
